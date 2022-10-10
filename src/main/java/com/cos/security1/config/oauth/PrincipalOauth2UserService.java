6bea932b2d075d7c9c8733678952bb151cc88d43
JeongChanWoo <ftfgz5093@gmail.com>
2022-10-10 22:54:54 +0900

    페이스북 로그인 완료

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,10 @@
 package com.cos.security1.config.oauth;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.ROLE;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -30,24 +34,28 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         // 구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인 완료 -> code 리턴(OAuth-Client라이브러리) ->AccessToken요청
         // userRequest 정보 -> loadUser함수 호출 -> 구글로부터 회원 프로필 받아준다
-        System.out.println("getAdditionalParameters : " + userRequest.getAdditionalParameters());
+        System.out.println("oAuth2User (getAttributes) : " + oAuth2User.getAttributes());
 
         // 회원가입 강제로 진행
+        OAuth2UserInfo oAuth2UserInfo = null;
         if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
-            // 수정 필요
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
         }
         if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
-            // 수정 필요
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
         }
         if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
-            // 수정 필요
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo(oAuth2User.getAttributes());
         }
 
-        String provider = userRequest.getClientRegistration().getRegistrationId(); //google
-        String providerId = oAuth2User.getAttribute("sub");
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider+"_"+providerId; //google_12321420140
         String password = passwordEncoder.encode("겟인데어");
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         ROLE role = ROLE.ROLE_USER;
 
         User userEntity = userRepository.findByUsername(username);
